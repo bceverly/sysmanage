6a5b18e9453b08d7584608c755d4b47b3a2b00ea
bceverly <bryan@theeverlys.com>
2024-05-01 13:01:04 -0400

    Fixed pylint issues

--- api/login.py
+++ api/login.py
@@ -1,14 +1,24 @@
-from fastapi import FastAPI, HTTPException, APIRouter
+"""
+This module provides the necessary function to support login to the SysManage
+server.
+"""
+from fastapi import HTTPException, APIRouter
 from pydantic import BaseModel
 
 router = APIRouter()
 
 class UserLogin(BaseModel):
+    """
+    This class represents the payload to the /login POST request.
+    """
     userid: str
     password: str
 
 @router.post("/login")
 async def login(login_data: UserLogin):
+    """
+    This function provides login ability to the SysManage server.
+    """
     if login_data.userid == 'user':
         if login_data.password == 'password':
             return { "message": "success", "result": "true" }
