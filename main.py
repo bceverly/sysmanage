b8d19fc955e4d40c680a5e9edce6363d31d52897
bceverly <bryan@theeverlys.com>
2024-05-02 09:15:29 -0400

    Added config module

--- main.py
+++ main.py
@@ -4,39 +4,25 @@ process.  It reads and processes the sysmanage.yaml configuration file, sets
 up the CORS configuration in the middleware, includes all of the various
 routers for the system and then launches the application.
 """
-import sys
 import uvicorn
-import yaml
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from api import login
+from config import config
 
-# Read/validate the configuration file
-try:
-    with open('sysmanage.yaml', 'r', encoding="utf-8") as file:
-        config = yaml.safe_load(file)
-        if not 'hostName' in config.keys():
-            config['hostName'] = "localhost"
-        if not 'apiPort' in config.keys():
-            config['apiPort'] = 8000
-        if not 'webPort' in config.keys():
-            config['webPort'] = 8080
-except yaml.YAMLError as exc:
-    if hasattr(exc, 'problem_mark'):
-        mark = exc.problem_mark
-        print (f"Error reading sysmanage.yaml on line {mark.line+1} in column {mark.column+1}")
-        sys.exit(1)
+# Parse the /etc/sysmanage.yaml file
+app_config = config.get_config()
 
 # Start the application
 app = FastAPI()
 
 # Set up the CORS configuration
 origins = [
-    "http://"+config['hostName'],
-    "http://"+config['hostName']+":"+str(config['webPort']),
-    "http://"+config['hostName']+":"+str(config['apiPort']),
+    "http://"+app_config['hostName'],
+    "http://"+app_config['hostName']+":"+str(app_config['webPort']),
+    "http://"+app_config['hostName']+":"+str(app_config['apiPort']),
 ]
 app.add_middleware(
     CORSMiddleware,
@@ -58,4 +44,4 @@ async def root():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=config['hostName'], port=config['apiPort'])
+    uvicorn.run(app, host=app_config['hostName'], port=app_config['apiPort'])
