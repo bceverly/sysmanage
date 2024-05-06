197872e9711d83ff7327a68506dd60b770b55dd0
bceverly <bryan@theeverlys.com>
2024-05-06 10:11:25 -0400

    Added PUT

--- api/user.py
+++ api/user.py
@@ -4,7 +4,6 @@ This module contains the API implementation for the user object in the system.
 from datetime import datetime, timezone
 from fastapi import HTTPException, APIRouter
 from pydantic import BaseModel
-from sqlalchemy import select, delete
 from sqlalchemy.orm import sessionmaker
 
 from pyargon2 import hash as argon2_hash
@@ -20,7 +19,6 @@ class User(BaseModel):
     """
     userid: str
     password: str
-    last_access: datetime
 
 @router.delete("/user/{id}")
 async def delete_user(id: int):
@@ -32,7 +30,15 @@ async def delete_user(id: int):
     session_local = sessionmaker(autocommit=False, autoflush=False, bind=db.get_engine())
 
     with session_local() as session:
-        session.execute(delete(models.User).where(models.User.id == id))
+        # See if we were passed a valid id
+        users = session.query(models.User).filter(models.User.id == id).all()
+
+        # Check for failure
+        if len(users) != 1:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        # Delete the record
+        session.query(models.User).filter(models.User.id == id).delete()
         session.commit()
 
     return { "message": "success", "result": "true" }
@@ -47,8 +53,7 @@ async def get_user(id: int):
     session_local = sessionmaker(autocommit=False, autoflush=False, bind=db.get_engine())
 
     with session_local() as session:
-        result = session.execute(select(models.User).where(models.User.id == id))
-        users = result.mappings().all()
+        users = session.query(models.User).filter(models.User.id == id).all()
 
         # Check for failure
         if len(users) != 1:
@@ -66,11 +71,13 @@ async def get_user_by_userid(userid: str):
     session_local = sessionmaker(autocommit=False, autoflush=False, bind=db.get_engine())
 
     with session_local() as session:
-        result = session.execute(select(models.User).where(models.User.userid == userid))
-        users = result.mappings().all()
-        return { "message": "success", "result": users }
+        users = session.query(models.User).filter(models.User.userid == userid).all()
 
-    return { "message": "success", "result": "true" }
+        # Check for failure
+        if len(users) == 0:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        return { "message": "success", "result": users }
 
 @router.get("/users")
 async def get_all_users():
@@ -106,3 +113,33 @@ async def add_user(new_user: User):
         session.commit()
     
     return { "message": "success", "result": user }
+
+@router.put("/user/{id}")
+async def update_user(id: int, user_data: User):
+    """
+    This function updates an existing user by id
+    """
+
+    # Get the /etc/sysmanage.yaml configuration
+    the_config = config.get_config()
+
+    # Get the SQLAlchemy session
+    session_local = sessionmaker(autocommit=False, autoflush=False, bind=db.get_engine())
+
+    # Update the user
+    with session_local() as session:
+        # See if we were passed a valid id
+        users = session.query(models.User).filter(models.User.id == id).all()
+
+        # Check for failure
+        if len(users) != 1:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        # Update the values
+        hashed_value = argon2_hash(user_data.password, the_config["security"]["password_salt"])
+        session.query(models.User).filter(models.User.id == id).update({models.User.userid: user_data.userid, 
+                                                                        models.User.hashed_password: hashed_value, 
+                                                                        models.User.last_access: datetime.now(timezone.utc)})
+        session.commit()
+
+    return { "message": "success", "result": "true" }
