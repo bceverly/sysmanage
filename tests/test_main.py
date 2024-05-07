1a1581f02987d722db42e4d81b1029e1dd2c0de0
bceverly <bryan@theeverlys.com>
2024-05-07 15:38:05 -0400

    Got tests passing

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,4 +13,4 @@ def test_read_main():
     """
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+    assert response.json() == {"message": "Hello World"}
