c9857e8b07a6eba596dcb2eb6395b7a5d4594c5b
bceverly <bryan@theeverlys.com>
2024-05-07 15:38:05 -0400

    Got tests passing

--- backend/persistence/db.py
+++ backend/persistence/db.py
@@ -4,7 +4,7 @@ ORM used by SysManage.
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 from backend.config import config
 
