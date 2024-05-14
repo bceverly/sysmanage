37ddef7e57e5dd9258be873d373d36a3d176fea2
bceverly <bryan@theeverlys.com>
2024-05-14 11:01:03 -0400

    Added initial host table

--- backend/persistence/models.py
+++ backend/persistence/models.py
@@ -6,6 +6,28 @@ PostgreSQL database.
 from sqlalchemy import Column, BigInteger, String, DateTime, Boolean
 from backend.persistence.db import Base
 
+class BearerToken(Base):
+    """
+    This class holds the object mapping for the bearer token table in the
+    PostgreSQL database.
+    """
+    __tablename__ = "bearer_token"
+    token = Column(String(200), primary_key=True, index=True)
+    created_datetime = Column(DateTime)
+
+class Host(Base):
+    """
+    This class holds the object mapping for the user table in the PostgreSQL
+    database.
+    """
+    __tablename__ = "host"
+    id = Column(BigInteger, primary_key=True, index=True)
+    active = Column(Boolean, unique=False, index=False)
+    fqdn = Column(String, index=True)
+    ipv4 = Column(String)
+    ipv6 = Column(String)
+    last_access = Column(DateTime)
+
 class User(Base):
     """
     This class holds the object mapping for the user table in the PostgreSQL
@@ -17,12 +39,3 @@ class User(Base):
     userid = Column(String)
     hashed_password = Column(String, unique=False, index=False)
     last_access = Column(DateTime)
-
-class BearerToken(Base):
-    """
-    This class holds the object mapping for the bearer token table in the
-    PostgreSQL database.
-    """
-    __tablename__ = "bearer_token"
-    token = Column(String(200), primary_key=True, index=True)
-    created_datetime = Column(DateTime)
