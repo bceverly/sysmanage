6baf70bb076fb9f753c2c9a01db6b30699bf9990
bceverly <bryan@theeverlys.com>
2024-05-14 16:49:57 -0400

    Got login working

--- website/src/pages/signin.js
+++ website/src/pages/signin.js
@@ -1,12 +1,45 @@
 
 const SignIn = () => {
-    return (
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const email = event.target.elements.email.value;
+    const password = event.target.elements.password.value;
+    
+    fetch('https://api.sysmanage.org:8443/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        "userid": email,
+        "password": password
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+       .then((response) => response.json())
+       .then((data) => {
+          console.log(data);
+          // Handle data
+       })
+       .catch((err) => {
+          console.log(err.message);
+       });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
       <div>
-        <h1>Sign In</h1>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="text" />
       </div>
-    )
-  }
-  
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
   export default SignIn;
   
   
