d80058f24a594386d273c69439e80344023a3f1d
bceverly <bryan@theeverlys.com>
2024-05-30 11:29:26 -0400

    Hooked up user api

--- frontend/src/Pages/Users.tsx
+++ frontend/src/Pages/Users.tsx
@@ -1,15 +1,47 @@
 import { useNavigate } from "react-router-dom";
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+
+import { SysManageUser, doGetUsers } from '../Services/users'
+
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'userid', headerName: 'Email Address', width: 200 }
+]
 
 const Users = () => {
+    const [tableData, setTableData] = useState<SysManageUser[]>([]);
     const navigate = useNavigate();
 
     useEffect(() => {
         if (!localStorage.getItem('bearer_token')) {
             navigate("/login");
         }
-    });
-    return <div>Users</div>;
+        doGetUsers().then((response: SysManageUser[]) => {
+            setTableData(response);
+            console.log('Num users returned: ' + response.length);
+            console.log("typeof(response): " + typeof response);
+            for (let i=0 ; i<response.length ; i++) {
+                console.log(response[i]);
+            }
+            return Promise.resolve(response);
+        });
+    }, []);
+    return (
+        <div  style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={tableData}
+                columns={columns}
+                initialState={{
+                    pagination: {
+                      paginationModel: { page: 0, pageSize: 5 },
+                    },
+                  }}
+                  pageSizeOptions={[5, 10]}
+                  checkboxSelection
+            />
+        </div>
+    );
 }
  
 export default Users;
