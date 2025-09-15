407252539974118ae847873ea974be88a213c2dc
bceverly <bryan@theeverlys.com>
2025-09-15 08:32:50 -0400

    Fixed failing test

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -4,3 +4,22 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import './__tests__/setup';
+
+// Fix for React 18 Scheduler compatibility in JSDOM environment
+// This addresses the "Cannot read properties of undefined (reading 'S')" error
+global.IS_REACT_ACT_ENVIRONMENT = true;
+
+// Polyfill for React 18's scheduler in test environment
+Object.defineProperty(window, 'MessageChannel', {
+  writable: true,
+  value: class MessageChannel {
+    port1 = {
+      onmessage: null,
+      postMessage: () => {}
+    }
+    port2 = {
+      onmessage: null,
+      postMessage: () => {}
+    }
+  }
+});
